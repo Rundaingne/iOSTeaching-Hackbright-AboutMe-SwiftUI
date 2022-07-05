f5922d6bc4fd72ca3b0b7f0b6926401cd89b91b3
Rundaingne <48364843+Rundaingne@users.noreply.github.com>
2022-07-05 15:59:41 -0600

    Added about me profile section, including images and title/handle

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            
+            ZStack(alignment: .leading) {
+                Image("bannerImage")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Image("profilePicture")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 100, height: 100)
+                .clipShape(Circle())
+                .padding(.bottom, -90)
+                .offset(y: 40)
+            }
+            .padding(.bottom)
+            .padding(.bottom)
+            .padding(.bottom)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                HStack {
+                    Text("Erich Kumpunen")
+                        .font(.title)
+                        .fontWeight(.heavy)
+                    Image(systemName: "checkmark.seal.fill")
+                        .foregroundColor(Color.blue)
+                        .imageScale(.large)
+                }
+                
+                Text("@I_are_scientists")
+                    .font(.caption)
+                
+                //1
+                Text("My bio. Write something interesting about yourself! If you are an interesting person. Which I am not.")
+                    .fontWeight(.ultraLight)
+                    .padding(.top)
+            }
+            .padding(.horizontal)
+            Spacer()
+        }
+        //2
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewInterfaceOrientation(.portrait)
+        }
     }
 }
